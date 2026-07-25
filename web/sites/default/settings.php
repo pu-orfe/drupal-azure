bb682fd841ab733d1781b50f855288fb7d945d57
Michael Bino <bino@princeton.edu>
2026-07-24 21:00:38 -0400

    Initialize default Drupal 10 skeleton and switch pipeline to on-demand deployments

---
+++ web/sites/default/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * Main Drupal settings file.
+ */
+
+// This file is tracked in git and will be deployed.
+// It will have Azure Container Apps settings appended to it during the Docker build.
